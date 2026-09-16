9c4affa5577dedb14015cca9c6b954df926c77d3
Jade Shan <evokeralucard@gmail.com>
2026-09-16 16:01:15 +0800

    update

--- build.sh
+++ build.sh
@@ -1,39 +1,37 @@
 #!/bin/bash
 
-nvm use && sleep 3
-
 # ========================================
 # copy 3rd lib
 # ========================================
-# echo '--- start : copy 3rd lib ---'
-# mkdir -p webroot/3rd/
-# # rm -rf webroot/3rd/*	
-# # cp -r ~/workspace/nginx/jadecdn/webroot/3rd/jquery            ./webroot/3rd 
-# # cp -r ~/workspace/nginx/jadecdn/webroot/3rd/datatable         ./webroot/3rd 
-# # cp -r ~/workspace/nginx/jadecdn/webroot/3rd/bootstrap         ./webroot/3rd 
-# # cp -r ~/workspace/nginx/jadecdn/webroot/3rd/SyntaxHighlighter ./webroot/3rd 
-# # cp -r ~/workspace/nginx/jadecdn/webroot/3rd/showdown          ./webroot/3rd 
-# # cp -r ~/workspace/nginx/jadecdn/webroot/3rd/mathjax           ./webroot/3rd 
-# # cp -r ~/workspace/nginx/jadecdn/webroot/3rd/98.css            ./webroot/3rd 
-# # cp -r ~/workspace/nginx/jadecdn/webroot//3rd/mathjax          ./webroot/3rd 
-# echo '--- finish: copy 3rd lib ---'
+echo '--- start : copy 3rd lib ---'
+mkdir -p webroot/3rd/
+rm -rf webroot/3rd/*	
+cp -r ~/workspace/nginx/jadecdn/webroot/3rd/jquery            ./webroot/3rd 
+cp -r ~/workspace/nginx/jadecdn/webroot/3rd/datatable         ./webroot/3rd 
+cp -r ~/workspace/nginx/jadecdn/webroot/3rd/bootstrap         ./webroot/3rd 
+cp -r ~/workspace/nginx/jadecdn/webroot/3rd/SyntaxHighlighter ./webroot/3rd 
+cp -r ~/workspace/nginx/jadecdn/webroot/3rd/showdown          ./webroot/3rd 
+cp -r ~/workspace/nginx/jadecdn/webroot/3rd/mathjax           ./webroot/3rd 
+cp -r ~/workspace/nginx/jadecdn/webroot/3rd/98.css            ./webroot/3rd 
+cp -r ~/workspace/nginx/jadecdn/webroot/3rd/mathjax           ./webroot/3rd 
+echo '--- finish: copy 3rd lib ---'
 
 # ========================================
 # build gnuplot images
 # ========================================
-# echo '--- start : build math plot ---'
-# function gnuplotImage() {
-# 		echo " gnuplot \"$1\" > \"$1.png\" "
-# 		gnuplot "$1" > "$1.png"
-# }
-# function gnuplotDocImages() {
-# 	# for pltFile in ./docs/img-plt/*.plt;     do gnuplotImage "$pltFile"; done;
-# 	for pltFile in ./docs/img-plt/*/*.plt;   do gnuplotImage "$pltFile"; done;
-# 	for pltFile in ./docs/img-plt/*/*/*.plt; do gnuplotImage "$pltFile"; done;
-# }
-# # rm -rf ./docs/img-plt/**/*.png
-# gnuplotDocImages
-# echo '--- finish : build math plot ---'
+echo '--- start : build math plot ---'
+function gnuplotImage() {
+		echo " gnuplot \"$1\" > \"$1.png\" "
+		gnuplot "$1" > "$1.png"
+}
+function gnuplotDocImages() {
+	# for pltFile in ./docs/img-plt/*.plt;     do gnuplotImage "$pltFile"; done;
+	for pltFile in ./docs/img-plt/*/*.plt;   do gnuplotImage "$pltFile"; done;
+	for pltFile in ./docs/img-plt/*/*/*.plt; do gnuplotImage "$pltFile"; done;
+}
+# rm -rf ./docs/img-plt/**/*.png
+gnuplotDocImages
+echo '--- finish : build math plot ---'
 
 # ========================================
 # build plantuml images 
@@ -41,41 +39,41 @@ nvm use && sleep 3
 # https://plantuml.com/class-diagram
 # 
 # ========================================
-# echo '--- start : build uml ---'
-# mkdir -p ./docs/uml/out/
-# /opt/quickstart/plantuml-dir.sh -t svg -s ./docs/uml/src/ -o ./docs/uml/out/
-# echo '--- finish : build uml ---'
+echo '--- start : build uml ---'
+mkdir -p ./docs/uml/out/
+/opt/quickstart/plantuml-dir.sh -t svg -s ./docs/uml/src/ -o ./docs/uml/out/
+echo '--- finish : build uml ---'
 
 # ========================================
 # build css
 # ========================================
-# echo '--- start : build css ---'
-# npx gulp 'process-style-hobbit' 
-# npx gulp 'process-style-lo-fi' 
-# npx gulp 'process-style-paper-print' 
-# npx gulp 'process-style-window-ui' 
-#  npx gulp 'process-style-workout' 
-# npx gulp 'process-style-trpg' 
-# echo '--- finish : build css ---'
+echo '--- start : build css ---'
+npx gulp 'process-style-hobbit' 
+npx gulp 'process-style-lo-fi' 
+npx gulp 'process-style-paper-print' 
+npx gulp 'process-style-window-ui' 
+ npx gulp 'process-style-workout' 
+npx gulp 'process-style-trpg' 
+echo '--- finish : build css ---'
 
 
 # ========================================
 # compile javascript
 # ========================================
-echo '--- start : compile javascript ---'
-npx gulp compress-typescript
-echo '--- finish : compile javascript ---'
-# 
-# echo '--- start : compile typescript ---'
-# rm -rf webroot/scripts/ts/*
-# npx tsc -p tsconfig.json
-# echo '--- finish : compile typescript ---'
-# 
-# echo '--- start : minify javascript ---'
-# for f in resource basic dataStructure geo2d canvas web webHtmlPage 3rdLibTool wiki blog UIWindow sandtable testJadeTRPG testJadeUtils testJadeUI; do
-#   npx terser "webroot/scripts/ts/${f}.js" -o "webroot/scripts/ts/${f}.min.js"
-# done
-# echo '--- finish : minify javascript ---'
+# echo '--- start : compile javascript ---'
+# npx gulp compress-typescript
+# echo '--- finish : compile javascript ---'
+
+echo '--- start : compile typescript ---'
+rm -rf webroot/scripts/ts/*
+npx tsc -p tsconfig.json
+echo '--- finish : compile typescript ---'
+
+echo '--- start : minify javascript ---'
+for f in resource basic dataStructure geo2d canvas web webHtmlPage 3rdLibTool wiki blog UIWindow sandtable testJadeTRPG testJadeUtils testJadeUI; do
+  npx terser "webroot/scripts/ts/${f}.js" -o "webroot/scripts/ts/${f}.min.js"
+done
+echo '--- finish : minify javascript ---'
 
 
 # ========================================
@@ -89,11 +87,13 @@ echo '--- finish: copy html ---'
 # ========================================
 # docs
 # ========================================
-# echo '--- start : copy doc ---'
-# mkdir -p webroot/docs/
-# cp -r  docs/* webroot/docs/
-# echo '--- finish: copy doc ---'
+echo '--- start : copy doc ---'
+mkdir -p webroot/docs/
+cp -r  docs/* webroot/docs/
+echo '--- finish: copy doc ---'
 
+
+rm -rf ~/workspace/nginx/jadecdn/webroot/jadeutils.v3   
 cp -r webroot/* ~/workspace/nginx/jadecdn/webroot/jadeutils.v3   
 
 sleep 3 && sync
